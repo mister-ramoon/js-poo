27eda2f45722f5cae9e42c38ed09460a503b68b8
ramonruiz <jruiz@lightia.mx>
2023-05-24 09:40:27 -0600

    Clases en JavaScript

--- main.js
+++ main.js
@@ -31,3 +31,26 @@ const juanita = new Student("Juanita Alejandra", 15, [
   "Curso de Introducción a la Producción de Videojuegos",
   "Curso de Creación de Personajes",
 ]);
+
+// Prototipos con la sintaxis de clases
+class Student2 {
+  constructor({ name, age, cursosAprobados = [], email }) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+  }
+
+  aprobarCurso(nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+  }
+}
+
+const miguelito = new Student2({
+  email: "miguelito@platzi.com",
+  name: "Miguel",
+  age: 28,
+  cursosAprobados: [
+    "Curso de Análisis de Negocios para Ciencia de Datos",
+    "Curso de Introducción a la Producción de Videojuegos",
+  ],
+});
