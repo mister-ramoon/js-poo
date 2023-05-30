c0e150f8cd8d4a3cbef57ad65c31f4719a3bbb96
ramonruiz <jruiz@lightia.mx>
2023-05-30 10:36:11 -0600

    Módulos de ECMAScript 6

---
+++ module.mjs
@@ -0,0 +1,9 @@
+import { PlatziClass } from "./main.mjs";
+
+const clase80 = new PlatziClass({
+  name: "JavaScript: orientado a objetos",
+  videoID: "lPPgY3HLlhQ",
+});
+
+clase80.reproducir();
+clase80.pausar();
