b925fcce7fdcae56481a5cb4533f0bfd92517c7e
ramonruiz <jruiz@lightia.mx>
2023-05-31 10:36:14 -0600

    Herencia en JavaScript

--- main.mjs
+++ main.mjs
@@ -8,7 +8,7 @@ function videoStop(id) {
   console.log("Pausamos la url " + urlSecreta);
 }
 
-export class PlatziClass {
+class PlatziClass {
   constructor({ name, videoID }) {
     this.name = name;
     this.videoID = videoID;
@@ -93,4 +93,124 @@ const escuelaVgs = new LearingPath({
     "Curso de Unreal Engine",
     "Curso de Unity 3D",
   ],
+});
+
+class Course {
+  constructor({ name, classes = [], isFree = false, lang = "spanish" }) {
+    this.name = name;
+    this.classes = classes;
+    this.isFree = isFree;
+    this.lang = lang;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nuevoNombrecito) {
+    if (typeof nuevoNombrecito === "string") {
+      let nameCapitalized = nuevoNombrecito.split(" ");
+
+      for (let i = 0; i < nameCapitalized.length; i++) {
+        nameCapitalized[i] =
+          nameCapitalized[i][0].toUpperCase() + nameCapitalized[i].substr(1);
+      }
+
+      this._name = nameCapitalized.join(" ");
+    } else {
+      console.log("Tu nombre debe ser una cadena de texto");
+    }
+  }
+}
+
+const cursoProBasicaName = "curso gratis de programación básica";
+const cursoProgBasica = new Course({
+  name: cursoProBasicaName,
+  lang: "spanish",
+  isFree: true,
+});
+
+const cursoDefinitivoHTML = new Course({
+  name: "Curso Definitivo de HTML y CSS",
+  lang: "spanish",
+  isFree: false,
+});
+
+const englishCourse = new Course({
+  name: "English Course",
+  lang: "english",
+  isFree: false,
+});
+
+class Student {
+  constructor({
+    name,
+    email,
+    username,
+    twitter = undefined,
+    instagram = undefined,
+    facebook = undefined,
+    approvedCourses = [],
+    learningPaths = [],
+  }) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.socialMedia = {
+      twitter,
+      instagram,
+      facebook,
+    };
+    this.approvedCourses = approvedCourses;
+    this.learningPaths = learningPaths;
+  }
+}
+
+class FreeStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    if (newCourse.isFree) {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn(
+        "Lo sentimos, " + this.name + ", solo puedes tomar cursos abiertos"
+      );
+    }
+  }
+}
+
+class BasicStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    if (newCourse.lang !== "english") {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn(
+        "Lo sentimos, " + this.name + ", no puedes tomar cursos en inglés"
+      );
+    }
+  }
+}
+
+const juan = new FreeStudent({
+  name: "JuanDC",
+  username: "juandc",
+  email: "juan@platzi.com",
+  twitter: "fjuandc",
+  learningPaths: [escuelaWeb, escuelaVgs],
+});
+
+const miguelito = new BasicStudent({
+  name: "Miguelito",
+  username: "migelitofeliz",
+  email: "mighuel@platzi.com",
+  instagram: "migelito_feliz",
+  learningPaths: [escuelaWeb, escuelaData],
+  approvedCourses: [cursoProgBasica],
 });
